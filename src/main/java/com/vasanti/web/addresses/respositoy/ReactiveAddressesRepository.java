63bcfad0191e5cb71aa946186e5b34b7559f92f3
kiranbadi <kiran@vasanti.org>
2021-01-20 21:48:44 -0500

    Added spans on Repository class

--- src/main/java/com/vasanti/web/addresses/respositoy/ReactiveAddressesRepository.java
+++ src/main/java/com/vasanti/web/addresses/respositoy/ReactiveAddressesRepository.java
@@ -1,6 +1,7 @@
 package com.vasanti.web.addresses.respositoy;
 
 import com.vasanti.web.addresses.model.address;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
@@ -10,11 +11,14 @@ import reactor.core.publisher.Mono;
 public interface ReactiveAddressesRepository extends ReactiveMongoRepository<address, String>,
         CustomAddressesRespository {
 
+    @NewSpan
     @Query(value="{'addressid' : $0}", delete = true)
     Mono<Void> deleteByaddressid (Long addressid);
 
+
+    @NewSpan
     Flux<address> findAllByCity(String city, Pageable pageable);
 
-
+    @NewSpan
     Flux<address> findAllByState(String state,Pageable pageable);
 }
--- src/main/java/com/vasanti/web/addresses/service/AddressService.java
+++ src/main/java/com/vasanti/web/addresses/service/AddressService.java
@@ -1,6 +1,7 @@
 package com.vasanti.web.addresses.service;
 
 import com.vasanti.web.addresses.model.address;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -8,15 +9,34 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 public interface AddressService {
+    @NewSpan
     Mono<address> insert(address addr);
+
+    @NewSpan
     Flux<address> saveAll(List<address> cities);
+
+    @NewSpan
     Mono<address> findById(String id);
+
+    @NewSpan
     Mono<address> findByaddressid(Long addressid);
+
+    @NewSpan
     Flux<address> findAll();
+
+    @NewSpan
     Flux<address> findAllByState(String state,Pageable pageable);
+
+    @NewSpan
     Flux<address> findAllByCity(String city, Pageable pageable);
+
+    @NewSpan
     Mono<Void> deleteAll();
+
+    @NewSpan
     Mono<Void> deleteByaddressid(Long addressid);
+
+    @NewSpan
     Mono<Void> updateByaddressid(Long addressid);
 
 
